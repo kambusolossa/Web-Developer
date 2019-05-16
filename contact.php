a9516b1e58c5ddb239ad45bbf53579a660e14c18
kambusolossa <50686879+kambusolossa@users.noreply.github.com>
2019-05-15 20:22:31 -0700

    about.html blog_post.html contact.php index.html index.php fooplate_style.css

---
+++ contact.php
@@ -0,0 +1,37 @@
+<div id="tooplate_sidebar">
+        	<div class="sidbar_box">
+		    	<a href="#"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.4542165054154!2d112.76733241407942!3d-7.302757373817553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa568566a54b%3A0x2ff2f8d4ab63151c!2sSDN+Nginden+Jangkungan+I+247!5e0!3m2!1sid!2sid!4v1539863191453" width="250" height="250" frameborder="0" style="border:0" allowfullscreen></iframe></a>
+			</div>
+			
+            <div class="sidbar_box">
+		        <h4>Categories</h4>
+                <ul class="tooplate_list">
+                    <li><a href="index.html">Aliquam ut arcu a metus</a></li>
+                    <li><a href="index.html">Donec eu ipsum lectus</a></li>
+                    <li><a href="index.html">Fusce at mauris massa</a></li>
+                    <li><a href="index.html">Mauris eleifend elit nec</a></li>
+                    <li><a href="index.html">Phasellus posuere ipsum</a></li>
+                    <li><a href="index.html">Sed venenatis dui eget</a></li>
+                </ul>
+			</div>
+			
+            <div class="sidbar_box">
+		        <h4>Recent comments</h4>
+                <div class="recent_comment_box">
+                    <a href="#">Maecenas vel ligula dolor</a>
+                    <p>Curabitur pellentesque varius nibh, sit amet ultrices urna lobortis ac.</p>
+                </div>
+                
+                <div class="recent_comment_box">
+                    <a href="#">Aenean feugiat mattis </a>
+                    <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+                </div>
+                
+                <div class="recent_comment_box last_recent_comment_box">
+                    <a href="#"> Lacus enim id lacinia in</a>
+                    <p>Curabitur ullamcorper ultricies nisi.  Nam eget dui. Validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow"><strong>XHTML</strong></a> and <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow"><strong>CSS</strong></a>.</p>
+                </div>	
+			</div>
+			
+            <div class="cleaner"></div>
+        </div>
---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Elegant Theme - Free Website Template</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<!--
+Template 2030 Elegant
+http://www.tooplate.com/view/2030-elegant
+-->
+<link href="tooplate_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+<script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+$(window).load(function() {
+	$('#slider').nivoSlider({
+		effect:'random',
+		slices:15,
+		animSpeed:500,
+		pauseTime:3000,
+		startSlide:0, //Set starting Slide (0 index)
+		directionNav:false, 
+		directionNavHide:false, //Only show on hover
+		controlNav:false, //1,2,3...
+		controlNavThumbs:false, //Use thumbnails for Control Nav
+		pauseOnHover:true, //Stop animation while hovering
+		manualAdvance:false, //Force manual transitions
+		captionOpacity:0.8, //Universal caption opacity
+		beforeChange: function(){},
+		afterChange: function(){},
+		slideshowEnd: function(){} //Triggers after all slides have been shown
+	});
+});
+</script>
+
+</head>
+<body>
+
+<div id="tooplate_wrapper">
+	<div id="tooplate_header">
+        <div id="site_title"><h1><a href="#">Maybrat Organisasi Surabaya </a></h1></div>
+    </div> <!-- end of forever header -->
+    
+    <div id="tooplate_menu">
+        <ul>
+            <li><a href="index.html" class="current">Home</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="contact.html" class="last">Contact</a></li>
+        </ul>    	
+    </div> <!-- end of tooplate_menu -->
+    <!--
+   
+                  -->
+            <div class="cleaner"></div>
+
+        </div>
+        <!--file select kiri-->
+          <!--file tutup kiri-->
+        <!--
+        
+         -->
+        <div class="cleaner"></div>
+
+	</div> <!-- end of main -->
+   <!--
+    <div id="tooplate_footer_wrapper">
+        <div id="tooplate_footer">
+            
+            <div class="col_w280">
+                <h5>Pages</h5>
+                <ul class="tooplate_list">
+                    <li><a href="index.html">Etiam commodo</a></li>
+                    <li><a href="index.html">Praesent adipiscing</a></li>
+                    <li><a href="index.html">Duis sed justo</a></li>
+                    <li><a href="index.html">Mauris vulputate</a></li>
+                    <li><a href="index.html">Mauris dignissim</a></li>
+                    <li><a href="index.html">In adipiscing purus</a></li>
+                </ul>
+            </div>
+            
+            <div class="col_w280">
+                <h5>Partners</h5>
+                <ul class="tooplate_list">
+                    <li><a href="index.html">Aenean ac tellus nec</a></li>
+                    <li><a href="index.html">Maecenas vestibulum</a></li>
+                    <li><a href="index.html">Ut eget tellus eget</a></li>
+                    <li><a href="index.html">Curabitur ut lectus</a></li>
+                    <li><a href="index.html">Vivamus vel augue vel</a></li>
+                    <li><a href="index.html">Praesent varius tempor</a></li>
+                </ul>
+            </div>
+            
+            <div class="col_w280 last_col">
+                <h5>About Us</h5>
+                <p>Aliquam venenatis nisl a lacus facilisis accumsan. Ut id tellus a velit adipiscing condimentum ornare nec lectus. Nam ut dui sit amet metus sagittis bibendum quis in odio. Quisque sit amet erat a lorem condimentum placerat. Duis dignissim, justo vel consequat vulputate, neque nibh gravida nisi, et posuere mauris turpis dapibus quam.</p>
+            </div>
+        
+            <div class="cleaner"></div>
+        </div>
+        
+        <div id="tooplate_copyright">
+		
+            Copyright © 2048 <a href="#">Company Name</a> - Design: <a href="http://www.tooplate.com">tooplate</a>
+			
+        </div>
+	</div>
+-->
+</div> <!-- end of wrapper -->
+
+</body>
+</html>
